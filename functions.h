091ca2210fec761a8111f280cd2749d1992e9db6
sandrinaferani <sandrinaferani@gmail.com>
2022-06-22 15:03:44 +0800

    urutannya gini gmna?

--- functions.h
+++ functions.h
@@ -32,6 +32,12 @@ struct Todo *parse_todo(char *username);
 void insert_todo(char *username);
 void show_todo(char *username);
 
+
+
+void removeNewLine() {
+    fflush(stdin);
+}
+
 bool login(char *username, char *password) {
     int usernameHash = getHash(username);
     struct User *data = parse_user();
@@ -54,6 +60,126 @@ bool login(char *username, char *password) {
     }
     
     return false;
+}
+
+int getHash(char *username) {
+    int index = 0;
+    for(int i = 0; i < sizeof(username) / sizeof(username[0]); i++) {
+        index += username[i];
+    }
+    return index % MAX_USER;
+}
+
+bool signup(char *username, char *password) {
+    struct User *data = parse_user();
+    int usernameHash = getHash(username);
+
+    if(strcmp(data[usernameHash].username, username) == 0) return false;
+    else {
+        struct User *iterate = &(data[usernameHash]);
+        while(iterate->next != NULL) {
+            iterate = iterate->next;
+            if(strcmp(iterate->username, username) == 0) return false;
+        }
+    }
+
+    file_open = fopen("user_db.csv", "a+");
+    fprintf(file_open, "%s;%s;\n", username, password);
+    fclose(file_open);
+
+    char *filename = malloc(sizeof(username));
+    strcpy(filename, username);
+    strcat(filename, ".csv");
+    file_open = fopen(filename, "w");
+    fclose(file_open);
+    
+    return true;
+}
+
+struct User *parse_user() {
+    struct User *user_data = (struct User*)malloc(MAX_USER * sizeof(struct User));
+    for(int i = 0; i < MAX_USER; i++) {
+        user_data[i].used = false;
+        user_data[i].next = NULL;
+    }
+
+    file_open = fopen("user_db.csv", "r");
+    
+    if(!file_open) {
+        printf("\nData user tidak tersedia, aplikasi ditutup...\n");
+        system("pause");
+        exit(0);
+    } else {
+        while(fgets(buffer, 100, file_open)) {
+            // printf("%s\n", buffer);
+            char *value = strtok(buffer, ";");
+            // printf("%s\n", value);
+            int index = getHash(value);
+            if(user_data[index].used == false) {
+                user_data[index].used = true;
+                strcpy(user_data[index].username, value);
+                value = strtok(NULL, ";");
+                // printf("%s\n", value);
+                strcpy(user_data[index].password, value);
+            } else {
+                struct User *temp = (struct User*)malloc(sizeof(struct User));
+                struct User *iterate = &(user_data[index]);
+                temp->used = true;
+                temp->next = NULL;
+                strcpy(temp->username, value);
+                value = strtok(NULL, ";");
+                strcpy(temp->password, value);
+
+                while(iterate->next != NULL) {
+                    iterate = iterate->next;
+                }
+                iterate->next = temp;
+            }
+        }
+    }
+    fclose(file_open);
+    return user_data;
+}
+
+struct Todo *parse_todo(char *username) {
+    struct Todo *head = NULL;
+    struct Todo *tail = NULL;
+    
+    char *filename = malloc(sizeof(username));
+    strcpy(filename, username);
+    strcat(filename, ".csv");
+
+    file_open = fopen(filename, "r");
+
+    if(!file_open) {
+        printf("\nData user tidak tersedia, aplikasi ditutup...\n");
+        system("pause");
+        exit(0);
+    } else {
+        while(fgets(buffer, 100, file_open)) {
+            struct Todo *temp = (struct Todo*)malloc(sizeof(struct Todo));
+
+            char *value = strtok(buffer, ";");
+            strcpy(temp->activity, value);
+
+            value = strtok(NULL, ";");
+            strcpy(temp->date, value);
+
+            value = strtok(NULL, ";");
+            temp->status = *value;
+
+            temp->next = NULL;
+
+            if(head == NULL) head = tail = temp;
+            else {
+                tail->next = temp;
+                tail = temp;
+            }
+        }
+    }
+
+    fclose(file_open);
+    return head;
 }
 
 void insert_todo(char *username){
@@ -136,90 +262,6 @@ void show_todo(char *username){
         }
     }
     fclose(file_open);
-}
-
-
-struct User *parse_user() {
-    struct User *user_data = (struct User*)malloc(MAX_USER * sizeof(struct User));
-    for(int i = 0; i < MAX_USER; i++) {
-        user_data[i].used = false;
-        user_data[i].next = NULL;
-    }
-
-    file_open = fopen("user_db.csv", "r");
-    
-    if(!file_open) {
-        printf("\nData user tidak tersedia, aplikasi ditutup...\n");
-        system("pause");
-        exit(0);
-    } else {
-        while(fgets(buffer, 100, file_open)) {
-            // printf("%s\n", buffer);
-            char *value = strtok(buffer, ";");
-            // printf("%s\n", value);
-            int index = getHash(value);
-            if(user_data[index].used == false) {
-                user_data[index].used = true;
-                strcpy(user_data[index].username, value);
-                value = strtok(NULL, ";");
-                // printf("%s\n", value);
-                strcpy(user_data[index].password, value);
-            } else {
-                struct User *temp = (struct User*)malloc(sizeof(struct User));
-                struct User *iterate = &(user_data[index]);
-                temp->used = true;
-                temp->next = NULL;
-                strcpy(temp->username, value);
-                value = strtok(NULL, ";");
-                strcpy(temp->password, value);
-
-                while(iterate->next != NULL) {
-                    iterate = iterate->next;
-                }
-                iterate->next = temp;
-            }
-        }
-    }
-    fclose(file_open);
-    return user_data;
-}
-
-int getHash(char *username) {
-    int index = 0;
-    for(int i = 0; i < sizeof(username) / sizeof(username[0]); i++) {
-        index += username[i];
-    }
-    return index % MAX_USER;
-}
-
-void removeNewLine() {
-    fflush(stdin);
-}
-
-bool signup(char *username, char *password) {
-    struct User *data = parse_user();
-    int usernameHash = getHash(username);
-
-    if(strcmp(data[usernameHash].username, username) == 0) return false;
-    else {
-        struct User *iterate = &(data[usernameHash]);
-        while(iterate->next != NULL) {
-            iterate = iterate->next;
-            if(strcmp(iterate->username, username) == 0) return false;
-        }
-    }
-
-    file_open = fopen("user_db.csv", "a+");
-    fprintf(file_open, "%s;%s;\n", username, password);
-    fclose(file_open);
-
-    char *filename = malloc(sizeof(username));
-    strcpy(filename, username);
-    strcat(filename, ".csv");
-    file_open = fopen(filename, "w");
-    fclose(file_open);
-    
-    return true;
 }
 
 void general_finish(char *username) {
@@ -317,43 +359,3 @@ void strict_finish(char *username) {
     system("pause");
 }
 
-struct Todo *parse_todo(char *username) {
-    struct Todo *head = NULL;
-    struct Todo *tail = NULL;
-    
-    char *filename = malloc(sizeof(username));
-    strcpy(filename, username);
-    strcat(filename, ".csv");
-
-    file_open = fopen(filename, "r");
-
-    if(!file_open) {
-        printf("\nData user tidak tersedia, aplikasi ditutup...\n");
-        system("pause");
-        exit(0);
-    } else {
-        while(fgets(buffer, 100, file_open)) {
-            struct Todo *temp = (struct Todo*)malloc(sizeof(struct Todo));
-
-            char *value = strtok(buffer, ";");
-            strcpy(temp->activity, value);
-
-            value = strtok(NULL, ";");
-            strcpy(temp->date, value);
-
-            value = strtok(NULL, ";");
-            temp->status = *value;
-
-            temp->next = NULL;
-
-            if(head == NULL) head = tail = temp;
-            else {
-                tail->next = temp;
-                tail = temp;
-            }
-        }
-    }
-
-    fclose(file_open);
-    return head;
-}
